1f5a513e6713ed9193cfa1f086117d359c706d8a
Josh Soref <jsoref@users.noreply.github.com>
2020-05-20 21:09:17 -0400

    spelling: highlighted

--- src/modules/fancyzones/lib/FancyZones.h
+++ src/modules/fancyzones/lib/FancyZones.h
@@ -77,7 +77,7 @@ interface __declspec(uuid("{5C8D99D6-34B2-4F4A-A8E5-7483F6869775}")) IZoneWindow
      */
     IFACEMETHOD_(IZoneWindow*, GetParentZoneWindow) (HMONITOR monitor) = 0;
     /**
-     * @returns Integer in range [0, 100] indicating opacity of highlited zone (while giving zone layout hints).
+     * @returns Integer in range [0, 100] indicating opacity of highlighted zone (while giving zone layout hints).
      */
     IFACEMETHOD_(int, GetZoneHighlightOpacity)() = 0;
     /**
