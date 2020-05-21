0933b88a343511300092bd333786b698a5013a10
Josh Soref <jsoref@users.noreply.github.com>
2020-05-20 21:06:32 -0400

    spelling: constructor

--- src/common/animation.h
+++ src/common/animation.h
@@ -3,7 +3,7 @@
 
 /*
   Usage:
-    When creating animation contstructor takes one parameter - how long
+    When creating animation constructor takes one parameter - how long
     should the animation take in seconds.
 
     Call reset() when starting animation.
